23fcce9cc3284faf3107ea3ab44fbe4fbfdd9af9
Maxint R&D <mm_rd@maxint.nl>
2020-10-22 09:54:39 +0200

    Pinouts multi-io PCB staan in mxD1_multi.h

---
+++ ESP8266/sketchbook/mmD1-RobotFSBrowser/mmD1-RobotFSBrowser.ino
@@ -0,0 +1,357 @@
+/*
+  FSWebServer - Example WebServer with SPIFFS backend for esp8266
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the ESP8266WebServer library for Arduino environment.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  upload the contents of the data folder with MkSPIFFS Tool ("ESP8266 Sketch Data Upload" in Tools menu in Arduino IDE)
+  or you can upload the contents of a folder if you CD in that folder and run the following command:
+  for file in `\ls -A1`; do curl -F "file=@$PWD/$file" esp8266fs.local/edit; done
+
+  access the sample web page at http://esp8266fs.local
+  edit the page by going to http://esp8266fs.local/edit
+*/
+
+/*
+ * Virtual joystick funcionality:
+ *    https://automatedhome.party/2017/07/15/wifi-controlled-car-with-a-self-hosted-htmljs-joystick-using-a-wemos-d1-miniesp8266/
+ *    based on http://jeromeetienne.github.io/virtualjoystick.js/examples/basic.html
+ *    source: https://github.com/jeromeetienne/virtualjoystick.js
+ * 
+ * Websocket functionality:
+ *    https://github.com/Links2004/arduinoWebSockets
+ *    https://www.instructables.com/id/WiFi-WebSocket-Remote-Robot/
+ *    https://github.com/moononournation/ESPWebSocketRemote
+ *    
+ * MDNS multicast domain name system:
+ *    https://tttapa.github.io/ESP8266/Chap08%20-%20mDNS.html
+ */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+
+#define DBG_OUTPUT_PORT Serial
+
+#include <WiFiManager.h>
+
+#ifndef STASSID
+#define STASSID "your-ssid"
+#define STAPSK  "your-key"
+#endif
+//const char* ssid = STASSID;
+//const char* password = STAPSK;
+const char* host = "esp8266fs";     // mDNS name: <host>.local (not supported on Android!)
+
+ESP8266WebServer server(80);
+
+#include <WebSocketsServer.h>
+WebSocketsServer webSocket = WebSocketsServer(81);
+String fragmentBuffer = "";   // to hold stringfragments for longer Websocket commands
+
+////////////////////////////////////////////////////////
+// mx D1 multi-io functions
+#include "mxD1_multi.h"
+
+
+////////////////////////////////////////////////////////
+// FSBrowser
+#include "FSBrowser.h"
+
+
+////////////////////////////////////////////////////////
+// Motor remote control via virtual joystick or other device
+void motorRemote(int x, int y)
+{  // engage the motor based on received remote control commands where x, y range between -100 and 100
+  x=x*10;
+  y=y*10;
+  boolean yDir;
+  int aSpeed = abs(y);
+  int bSpeed = abs(y);
+
+  //set the direction based on y being negative or positive
+  if ( y < 0 ){
+    yDir = 0; 
+  }
+  else { 
+    yDir = 1;
+  }  
+  //adjust to speed of each each motor depending on the x-axis
+  //it slows down one motor and speeds up the other proportionately 
+  //based on the amount of turning
+  aSpeed = constrain(aSpeed + x/2, 0, 1023);
+  bSpeed = constrain(bSpeed - x/2, 0, 1023);
+
+  DBG_OUTPUT_PORT.printf("Motor remote: %s %d-%d, %d-%d\n", (yDir?"fwd":"bck"), x, aSpeed, y, bSpeed);
+  mxD1_multi_motors(yDir, aSpeed, bSpeed);
+/*
+  //use the speed and direction values to turn the motors
+  //if either motor is going in reverse from what is expected,
+  //just change the 2 digitalWrite lines for both motors:
+  //!ydir would become ydir, and ydir would become !ydir
+  digitalWrite(STBY, HIGH);  
+  //MotorA
+  digitalWrite(AIN1, !yDir);
+  digitalWrite(AIN2, yDir);
+  analogWrite(PWMA, aSpeed);
+  //MotorB
+  digitalWrite(BIN1, !yDir);
+  digitalWrite(BIN2, yDir);
+  analogWrite(PWMB, bSpeed);
+*/
+}
+
+//////////////////////////////////////////////////////////////////
+// handle commands received via javascript Ajax over HTTP
+//This function takes the parameters passed in the URL(the x and y coordinates of the joystick)
+//and sets the motor speed based on those parameters. 
+// format: ./jsData.html?x=12&y=34
+void handleJSData()
+{
+  int x = server.arg(0).toInt();
+  int y = server.arg(1).toInt();
+  DBG_OUTPUT_PORT.printf("JS: Joy %d, %d\n", x, y);
+  motorRemote(x,y);
+
+  //return an HTTP 200
+  server.send(200, "text/plain", "");   
+}
+
+
+
+//////////////////////////////////////////////////////////////////
+// Handle commands received via websockets (much faster than via HTTP Ajax)
+// [@]. Motor control - format: @x,y
+//
+//
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
+{
+  switch(type) {
+    case WStype_DISCONNECTED:
+      DBG_OUTPUT_PORT.printf("WS: [%u] Disconnected!\n", num);
+      // TODO: set all robot actions off
+      motorRemote(0,0);
+      break;
+
+    case WStype_CONNECTED: {
+      IPAddress ip = webSocket.remoteIP(num);
+      DBG_OUTPUT_PORT.printf("WS: [%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+      // send message to client
+      webSocket.sendTXT(num, "Connected");
+      display.println("CONN");
+      //TM1637PrintScrolled("Connected ");
+      playTune("T240 L8 O7 C r r C r16 E r16 F");
+    }
+      break;
+
+    case WStype_TEXT:
+      DBG_OUTPUT_PORT.printf("WS: [%u] get Text: %s\n", num, payload);
+      if (payload[0] == '@')
+      { // We received a virtual joystick command. Format: @x,y
+        // parse payload to find x,y
+        int x = atoi((const char *)payload+1);
+        char * pComma=strchr((const char *)payload, ',');
+        int y = pComma!=NULL ? atoi((const char *)pComma+1) : 0;
+        DBG_OUTPUT_PORT.printf("WS: Joy %d, %d\n", x, y);
+        motorRemote(x,y);
+      }
+      break;
+
+    case WStype_BIN:
+      DBG_OUTPUT_PORT.printf("WS: [%u] get binary length: %u\n", num, length);
+      hexdump(payload, length);
+      break;
+
+    // Fragmentation / continuation opcode handling
+    // case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT_TEXT_START:
+      fragmentBuffer = (char*)payload;
+      DBG_OUTPUT_PORT.printf("WS: [%u] get start start of Textfragment: %s\n", num, payload);
+      break;
+    case WStype_FRAGMENT:
+      fragmentBuffer += (char*)payload;
+      DBG_OUTPUT_PORT.printf("WS: [%u] get Textfragment : %s\n", num, payload);
+      break;
+    case WStype_FRAGMENT_FIN:
+      fragmentBuffer += (char*)payload;
+      DBG_OUTPUT_PORT.printf("WS: [%u] get end of Textfragment: %s\n", num, payload);
+      DBG_OUTPUT_PORT.printf("WS: [%u] full frame: %s\n", num, fragmentBuffer.c_str());
+      break;
+  }
+
+}
+
+
+void configModeCallback (WiFiManager *myWiFiManager)
+{
+  DBG_OUTPUT_PORT.println(F("Entered config mode"));
+  DBG_OUTPUT_PORT.println(WiFi.softAPIP());
+  DBG_OUTPUT_PORT.println(myWiFiManager->getConfigPortalSSID());
+  display.println("Conf");
+}
+
+
+
+
+void setup(void)
+{
+  DBG_OUTPUT_PORT.begin(115200);
+  DBG_OUTPUT_PORT.print("\n");
+  DBG_OUTPUT_PORT.print("mxD1-Robot FS/WS/vJoy.\n");
+  DBG_OUTPUT_PORT.setDebugOutput(true);   // enable printf and debug output from WiFi libraries
+
+  mxD1_multi_setup();
+  
+  SPIFFS.begin();
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    DBG_OUTPUT_PORT.printf("\n");
+  }
+
+
+  //WIFI INIT
+/*
+  DBG_OUTPUT_PORT.printf("Connecting to %s\n", ssid);
+  if (String(WiFi.SSID()) != String(ssid)) {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+  }
+*/
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  DBG_OUTPUT_PORT.printf("Starting WiFiManager for %s\n", host);
+  display.println("IP=");  
+  WiFiManager wifiManager;
+  //reset saved settings
+  //wifiManager.resetSettings();
+  //wifiManager.autoConnect(MDNS_NAME);
+  wifiManager.setDebugOutput(false);
+  wifiManager.setAPCallback(configModeCallback);
+  wifiManager.autoConnect(host);
+  //or use this for auto generated name ESP + ChipID
+  //wifiManager.autoConnect();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    DBG_OUTPUT_PORT.print(".");
+  }
+  DBG_OUTPUT_PORT.println("");
+  DBG_OUTPUT_PORT.print(F("Connected! IP address: "));
+  IPAddress ipAddress = WiFi.localIP();
+  DBG_OUTPUT_PORT.println(ipAddress);
+  display.println(ipAddress[3]);
+
+
+
+/*
+  if(MDNS.begin(host))
+    DBG_OUTPUT_PORT.printf("MDNS responder started for %s\n", host);
+  else
+    DBG_OUTPUT_PORT.printf("MDNS responder NOT started for %\n", host);
+  DBG_OUTPUT_PORT.print("Open http://");
+  DBG_OUTPUT_PORT.print(host);
+  DBG_OUTPUT_PORT.println(".local/edit to see the file browser");
+*/
+
+  //SERVER INIT
+  //list directory
+  server.on("/list", HTTP_GET, handleFileList);
+  //load editor
+  server.on("/edit", HTTP_GET, []() {
+    if (!handleFileRead("/edit.htm")) {
+      server.send(404, "text/plain", "FileNotFound");
+    }
+  });
+  //create file
+  server.on("/edit", HTTP_PUT, handleFileCreate);
+  //delete file
+  server.on("/edit", HTTP_DELETE, handleFileDelete);
+  //first callback is called after the request has ended with all parsed arguments
+  //second callback handles file uploads at that location
+  server.on("/edit", HTTP_POST, []() {
+    server.send(200, "text/plain", "");
+  }, handleFileUpload);
+
+  //set the static pages on SPIFFS for the html and js
+  //server.serveStatic("/", SPIFFS, "/joystick.html"); 
+  //server.serveStatic("/virtualjoystick.js", SPIFFS, "/virtualjoystick.js");
+  //call handleJSData function when this URL is accessed by the js in the html file
+  server.on("/jsData.html", handleJSData); 
+
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri())) {
+      server.send(404, "text/plain", "FileNotFound");
+    }
+  });
+
+  //get heap status, analog input value and all GPIO statuses in one json call
+  server.on("/all", HTTP_GET, []() {
+    String json = "{";
+    json += "\"heap\":" + String(ESP.getFreeHeap());
+    json += ", \"analog\":" + String(analogRead(A0));
+    json += ", \"gpio\":" + String((uint32_t)(((GPI | GPO) & 0xFFFF) | ((GP16I & 0x01) << 16)));
+    json += "}";
+    server.send(200, "text/json", json);
+    json = String();
+  });
+  server.begin();
+  DBG_OUTPUT_PORT.println(F("HTTP server started"));
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  DBG_OUTPUT_PORT.println(F("Websocket server started"));
+
+/*
+  if (MDNS.begin(MDNS_NAME)) {
+    USE_SERIAL.println("MDNS responder started");
+  }
+*/
+
+  // start Multicast domain name system for <host>.local (not supported on Android)
+  if(MDNS.begin(host))
+    DBG_OUTPUT_PORT.printf("MDNS responder started for %s\n", host);
+  else
+    DBG_OUTPUT_PORT.printf("MDNS responder NOT started for %\n", host);
+
+  // Add service to MDNS
+  MDNS.addService("http", "tcp", 80);
+  MDNS.addService("ws", "tcp", 81);
+
+  DBG_OUTPUT_PORT.print(F("Open http://"));
+  DBG_OUTPUT_PORT.print(host);
+  DBG_OUTPUT_PORT.println(F(".local/edit to see the file browser"));
+
+  //display.println("done");
+  rgbSet(1, RGB_NONE);
+  rgbSet(2, RGB_NONE);
+}
+
+void loop(void)
+{
+  server.handleClient();
+  MDNS.update();
+  webSocket.loop();
+  mxD1_multi_loop();
+}
---
+++ ESP8266/sketchbook/mmD1-RobotFSBrowser/mxD1_multi.h
@@ -0,0 +1,441 @@
+/*
+ * Maxint R&D
+ * D1 Mini multi-I/O devboard
+ * 
+ * Development board for D1 mini featuring multiple I/O interfaces
+ * 
+ * Board layout:
+ *   /----------------------------------------------------\
+ *  / PWM1               Proto area 2              PWM2    \
+ *  |                                                       \
+ *  | PWR                                              I2C  |
+ *  |                                                       |
+ *  |       I/O                                       5V    |
+ *  | SW1   GV70          BUZ                               | 
+ *  |                 LDR                                   |
+ *  |      D1 PINS               7-seg                      |
+ *  |      D1 PINS               4-dig                      |
+ *  |                 I2C SER                               |
+ *  |   Proto area 1                        Proto area 3    |
+ *  |                                                       |
+ *  |      D1 PINS                                          |
+ *  |      D1 PINS                                          |
+ *  |                                                       |
+ *  | SW2    I2C                  SW3                       |
+ *  |        I2C                                       5V   |
+ *  |                                                       |
+ *  | PWR                                 RGB2          I2C |
+ *  |                    Proto area 4                 RGB1  /
+ *  \ PWM4                                         PWM3    / 
+ *   \----------------------------------------------------/
+ *   
+ * FEATURES/PINOUT
+ * 
+ * Pin  Function                 ESP-8266 Pin  Device
+ * TX   TXD                      TXD           BT-TX
+ * RX   RXD                      RXD           BT-RX
+ * A0   Analog input, max 3.3V   A0            LDR
+ * D0   IO, wakeup               GPIO16
+ * D1   IO, SCL                  GPIO5         SCL
+ * D2   IO, SDA                  GPIO4         SDA
+ * D3   IO, 10k Pull-up          GPIO0         SW2 (low for UART flash boot)
+ * D4   IO, 10k Pull-up, LED     GPIO2         SW1 (high for normal boot)
+ * D5   IO, SCK                  GPIO14        TM1637-DIO
+ * D6   IO, MISO                 GPIO12        TM1637-CLK
+ * D7   IO, MOSI                 GPIO13
+ * D8   IO, 10k Pull-down, SS    GPIO15        BUZ (low for normal boot)
+ * G    Ground                   GND
+ * 5V   5V                       -
+ * 3V3  3.3V                     3.3V
+ * RST  Reset                    RST
+ * 
+ * Note: On the D1 mini you indeed need to watch out how pins are used.
+ * D3 and D4 have pull-ups. These are fine to use for a pushbutton, but cannot be used for devices
+ * that pull the pin low. D8 has a pull down. It is okay when used to drive a buzzer through a transistor.
+ *       
+ * On-board I2C DEVICES
+ * PCA9685 - 0x40/0x70 - 16-channel, 12-bit PWM Fm+ I2C-bus LED controller (address 0x70 for call all, A0-A5 for 0x40-0x7F)
+ * PCF8575 - 0x20 - 16-BIT I2C AND SMBus I/O Expander (address 0x20, A0-A2 for 0x20-0x27)
+ * 
+ * On-board TM1637 LED&Key
+ * 4x7SEG A-H, DIG1-4
+ * RGB1 A-C, DIG5
+ * RGB2 A-C, DIG6
+ * SW3 Key 8-12 on K1 (0-4 on K2)
+ * 
+ * I2C scanner code from  from https://playground.arduino.cc/Main/I2cScanner
+ * 
+ */
+
+
+// The onboard PCA9685 chip has 16 PWM pins, which can be used to drive LEDs and servos directly. Motors can be driven via a motor driver board
+// PCA9685 - 0x40/0x70 - 16-channel, 12-bit PWM Fm+ I2C-bus LED controller (address 0x70 for call all, A0-A5 for 0x40-0x7F)
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+// you can also call it with a different address and I2C interface
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(&Wire, 0x40);
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); // called this way, it uses the default address 0x40
+
+// The onboard PCF8574 chip has 16 digital I/O pins, which can be used for sensors, LEDs, buttons and other I/O devices
+// PCF8575 - 0x20 - 16-BIT I2C AND SMBus I/O Expander (address 0x20, A0-A2 for 0x20-0x27)
+#include <mxUnifiedPCF8574.h>
+mxUnifiedPCF8575 unio = mxUnifiedPCF8575(0x20);     // use the PCF875 I2C output expander on address 0x20
+
+
+// The buzzer is connected via a transistor to D8
+// The MmlMusicPWM library uses a timer to play music without interruptions
+#define PIN_BUZZER D8
+#include <MmlMusicPWM.h>
+MmlMusicPWM music(PIN_BUZZER);
+
+// A light sensitive resistor is connected to A0
+#define LDR A0
+
+// The TM11637 chip is connected via a DIO and a CLK line
+// It drives a 4 x 7-segment display and separate RGB leds and can also scan for button presses
+#define PIN_DIO D5
+#define PIN_CLK D6
+
+#include <TM1637.h>
+#include <TM16xxDisplay.h>
+#include <TM16xxButtons.h>
+TM1637 module(PIN_DIO, PIN_CLK);
+TM16xxDisplay display(&module, 4);    // TM16xx object, 4 digits
+TM16xxButtons buttons(&module);
+
+#ifndef DBG_OUTPUT_PORT
+#define DBG_OUTPUT_PORT Serial
+#endif
+
+
+//----------------FIX ESP8266 ISSUE WITH TICKER AND DELAY -----------
+#define OPT_FIXDELAY 0
+#if OPT_FIXDELAY
+void msDelay(unsigned int nDuration)
+{ // replacement for delay() which in ESP8266 core seems to conflict usage of timer scheduler by Ticker which is used in MmlMusicPWM
+  uint32_t ulStart=millis();
+  while(millis() < ulStart+nDuration)
+    yield();
+}
+
+// Redefine delay() which seems to collide with Ticker used in MmlMusicPWM (when playing during delay).
+#define delay(x) msDelay(x)
+#endif
+//-----------------------------------------------------------
+
+
+void blink(int nTimes=1, int nDelay=100)
+{
+  pinMode(LED_BUILTIN, OUTPUT);     // D1-MINI: LED_BUILTIN=GPIO2/D4, 10k Pull-up, connected to SW1
+  for(int n=0; n<nTimes; n++)
+  {
+    digitalWrite(LED_BUILTIN, LOW);   // set LOW to switch led on
+    delay(nDelay);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(nDelay);
+  }
+  pinMode(LED_BUILTIN, INPUT);     // D1-MINI: LED_BUILTIN=GPIO2/D4, 10k Pull-up, connected to SW1
+}
+
+#define RGB_RED B001
+#define RGB_GREEN B010
+#define RGB_BLUE B100
+#define RGB_YELLOW B011
+#define RGB_AQUA B110
+#define RGB_MAGENTA B101
+#define RGB_WHITE B111
+#define RGB_NONE B000
+
+void rgbSet(byte nLed, byte nColor)
+{ // Two RGB-LEDs are connected to the TM1637 chip on segments A,B and C of digits 5 and 6
+  // Their color can be set using a 3-bit RGB value, eg. rgbSet(1, B010) for green on RGB1 or rgbSet(2, B101) for purple on RGB2
+  if(nLed<1 || nLed>2) return;
+  module.setSegments(nColor, nLed+3);
+}
+
+void playTune(const char *szPlay)
+{ // play a tune if nothing else is playing
+  if(!music.isPlaying())
+    music.play(szPlay);
+}
+
+// define some patterns to blink LEDs on the PCF ports P4-P12
+// format: speed, count, pattern[x], ...
+const byte pcfLoopPattern0[] PROGMEM = {100, 10, 0x80, 0, 0, 0, 0, 0x01, 0, 0, 0, 0};
+const byte pcfLoopPattern1[] PROGMEM = {200, 2, 0xAA, 0x55};
+const byte pcfLoopPattern2[] PROGMEM = {100, 14,
+  0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000,
+  0b01000000, 0b00100000, 0b00010000, 0b00001000, 0b00000100, 0b00000010};
+const byte pcfLoopPattern3[] PROGMEM = {100, 8, 0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
+const byte pcfLoopPattern4[] PROGMEM = {100, 8, 0b10000000, 0b01000000, 0b00100000, 0b00010000, 0b00001000, 0b00000100, 0b00000010, 0b00000001};
+const byte pcfLoopPattern5[] PROGMEM = {100, 4, 0b10000001, 0b01000010, 0b00100100, 0b00011000};
+const byte pcfLoopPattern6[] PROGMEM = {100, 4, 0b00011000, 0b00100100, 0b01000010, 0b10000001};
+
+const byte pcfLoopPattern7[] PROGMEM = {100, 8, 0b10000001, 0b01000010, 0b00100100, 0b00011000, 0b00011000, 0b00100100, 0b01000010, 0b10000001};
+
+const byte *pcfLoopPatterns[]={
+  pcfLoopPattern0,
+  pcfLoopPattern1,
+  pcfLoopPattern2,
+  pcfLoopPattern3,
+  pcfLoopPattern4,
+  pcfLoopPattern5,
+  pcfLoopPattern6,
+  pcfLoopPattern7,
+};
+
+byte pcfLoopPatternPlaying=0;
+byte pcfLoopPatternPosition=1;
+byte pcfLoopPatternSpeed=0;
+
+void pcfLedsShow(byte btLeds)
+{
+  for (uint8_t pin=4; pin<=11; pin++)
+  {
+    unio.digitalWrite(pin, btLeds&0x01);
+    btLeds>>=1;
+  }
+}
+
+void pcfLedsSetPattern(byte nPatternNum=0, byte nSpeed=0)
+{
+  pcfLoopPatternPlaying=nPatternNum;
+  pcfLoopPatternSpeed= (nSpeed==0 ? pgm_read_byte(pcfLoopPatterns[pcfLoopPatternPlaying]) : nSpeed);
+  byte nCount=pgm_read_byte(pcfLoopPatterns[pcfLoopPatternPlaying]+1);
+//  pcfLoopPatternPosition=1;
+  DBG_OUTPUT_PORT.printf("Led pattern %d [size %d], speed %d\n", nPatternNum, nCount, pcfLoopPatternSpeed);
+}
+
+void pcfLedsSetup()
+{   // use the 16-bit I/O expander to drive 8 leds on P4-P12
+  for (uint8_t n=0; n<8; n++)
+  {
+    pcfLedsShow(bit(n));
+    delay(10);
+    pcfLedsShow(0);
+  }
+  pcfLedsSetPattern(0);
+}
+
+unsigned long ulLedsStart=0;
+void pcfLedsLoop()
+{
+  if(millis()>ulLedsStart+pcfLoopPatternSpeed)
+  {
+    ulLedsStart=millis();
+    pcfLoopPatternPosition++;
+    if(pcfLoopPatternPosition>pgm_read_byte(pcfLoopPatterns[pcfLoopPatternPlaying]+1))
+      pcfLoopPatternPosition=1;
+  //byte nCount=pgm_read_byte(pcfLoopPatterns[pcfLoopPatternPlaying]+1);
+  //DBG_OUTPUT_PORT.printf("Led pattern%d [size %d]: speed %d, pos %d\n", pcfLoopPatternPlaying, nCount, pcfLoopPatternSpeed, pcfLoopPatternPosition);
+    pcfLedsShow(pgm_read_byte(pcfLoopPatterns[pcfLoopPatternPlaying]+pcfLoopPatternPosition+1));
+  }
+}
+
+
+
+//=============================
+
+bool TM1637PrintScrolled(const char *pszText=NULL, bool fRepeatText=false, uint16_t nTimeDelay=333)
+{ // print scrolled text, or continue printing the previous text until done
+  // return true if still more text to print, or false if done
+  static const char *pText=NULL;
+  static int nPos=0;
+  static long lTime=0;
+  static bool fRepeat=false;
+  static uint16_t nDelay=false;
+  #define NUM_DIGITS 4
+
+  //DBG_OUTPUT_PORT.print("TM1637PrintScrolled Pos:");
+  //if(pText!=NULL)
+  //   DBG_OUTPUT_PORT.print(nPos);
+  if(pszText!=NULL)
+  { // init new text
+    pText=pszText;
+    //nPos=NUM_DIGITS;    // start right
+    nPos=0;               // start left
+    lTime=0;
+    fRepeat=fRepeatText;
+    nDelay=nTimeDelay;
+    display.clear();
+    //display.println(pText);
+/*
+    DBG_OUTPUT_PORT.print(" - ");
+    DBG_OUTPUT_PORT.print(pszText);
+    DBG_OUTPUT_PORT.print("[");
+    DBG_OUTPUT_PORT.print(strlen(pText));
+    DBG_OUTPUT_PORT.print("]");
+    DBG_OUTPUT_PORT.println(".");
+*/
+    return(true);   // still printing
+  }
+  else
+  { // continue showing previous text
+    //DBG_OUTPUT_PORT.print("-");
+    if(pText==NULL)
+       return(false);   // nothing to print
+
+    if(millis()>lTime+nDelay)
+    {
+/*
+      Serial.print("~");
+    Serial.print("[");
+    Serial.print(nPos, DEC);
+    Serial.print("]");
+*/
+      lTime=millis();
+      if(nPos > (0-((int)strlen(pText))-1))   // on ESP8266: size_t is unsigned. Must cast strlen() to int to compare negative values
+      { // scroll to next position
+        display.setCursor(nPos);
+        display.println(pText);
+        nPos--;   // cursor position can be negative to scroll text by starting to print it outside of visible area
+        //Serial.println(">");
+        return(true);   // still printing
+      }
+
+      if(nPos == (0-((int)strlen(pText))-1))
+      { // At end of string. Repeat or stop.
+        display.clear();
+        if(fRepeat)
+          nPos=NUM_DIGITS;
+        else
+          pText=NULL;
+        //DBG_OUTPUT_PORT.println("!");
+        return(false);   // done printing
+      }
+    }
+    return(true);   // not finished printing
+  }
+}
+
+
+
+//=============================
+// PCA9685 PWM motor
+//=============================
+
+void mxD1_multi_setupPCA(byte nAddress=0x40)
+{   // setup the PCA9685 PWM driver
+  byte error;
+
+  DBG_OUTPUT_PORT.println(F("setup: PCA9685"));
+  Wire.begin();
+  Wire.beginTransmission(nAddress);
+  error = Wire.endTransmission();
+
+  if(error!=0)
+  { // print error
+    DBG_OUTPUT_PORT.print("No I2C device found at address 0x");
+    if (nAddress<16)
+      DBG_OUTPUT_PORT.print("0");
+    DBG_OUTPUT_PORT.print(nAddress,HEX);
+    DBG_OUTPUT_PORT.println("!");
+    return;
+  }
+
+  pwm.begin();
+  pwm.setPWMFreq(1000);  // use higher pwm frequency for motors (servo's use 60 Hz)
+
+  // set all pwm pins off
+  for (uint8_t pin=0; pin<15; pin++)
+    pwm.setPWM(pin, 0, 4096);       // turns pin fully off
+}
+
+void mxD1_multi_motors(bool fDir, int nSpeedA, int nSpeedB)
+{   // drive the motors connected to PWM pins 12-15
+  pwm.setPWMFreq(200);  // use lower pwm frequency
+  #define MOTOR_MIN 1000     // minimum speed needed to start (L9110 driver)
+
+  if(nSpeedA==0 && nSpeedB==0)
+  { // stop
+    DBG_OUTPUT_PORT.printf("PCA Motors off!\n");
+    pwm.setPWM(12, 0, 4096);         // turns pin fully off
+    pwm.setPWM(13, 0, 4096);         // turns pin fully off
+    pwm.setPWM(14, 0, 4096);         // turns pin fully off
+    pwm.setPWM(15, 0, 4096);         // turns pin fully off
+    rgbSet(1, RGB_NONE);
+    rgbSet(2, RGB_NONE);
+    pcfLedsSetPattern(2);
+    display.println("stop");
+    //TM1637PrintScrolled("stop");
+    return;
+  }
+//  nSpeedA = constrain(nSpeedA, MOTOR_MIN, 4095);
+//  nSpeedB = constrain(nSpeedB, MOTOR_MIN, 4095);
+
+  if(fDir)
+  {
+    DBG_OUTPUT_PORT.printf("PCA Motors fwd: %d, %d\n", nSpeedA, nSpeedB);
+    pwm.setPWM(12, 0, nSpeedA*4);    // turns pin partially on
+    pwm.setPWM(13, 0, 4096);         // turns pin fully off
+    pwm.setPWM(14, 0, nSpeedB*4);    // turns pin partially on
+    pwm.setPWM(15, 0, 4096);         // turns pin fully off
+    rgbSet(1, (nSpeedA-nSpeedB)>50 ? RGB_GREEN : RGB_YELLOW);
+    rgbSet(2, (nSpeedB-nSpeedA)>50 ? RGB_GREEN : RGB_YELLOW);
+    pcfLedsSetPattern((nSpeedA-nSpeedB)>50 ? 3 : ((nSpeedB-nSpeedA)>50 ? 4 : 5), 255-((nSpeedA+nSpeedB)/10)%255);
+    display.println("for");
+    //TM1637PrintScrolled("for");
+  }
+  else
+  {
+    DBG_OUTPUT_PORT.printf("PCA Motors bck: %d, %d\n", nSpeedA, nSpeedB);
+    pwm.setPWM(12, 0, 4096);         // turns pin fully off
+    pwm.setPWM(13, 0, nSpeedA*4);    // turns pin partially on
+    pwm.setPWM(14, 0, 4096);         // turns pin fully off
+    pwm.setPWM(15, 0, nSpeedB*4);    // turns pin partially on
+    rgbSet(1, (nSpeedA-nSpeedB)>50 ? RGB_RED : RGB_MAGENTA);
+    rgbSet(2, (nSpeedB-nSpeedA)>50 ? RGB_RED : RGB_MAGENTA);
+    pcfLedsSetPattern((nSpeedA-nSpeedB)>50 ? 3 : ((nSpeedB-nSpeedA)>50 ? 4 : 6), 255-((nSpeedA+nSpeedB)/10)%255);
+    display.println("bck");
+    //TM1637PrintScrolled("bck");
+  }
+}
+
+
+
+//=============================
+
+void fnClick(byte nButton)
+{ // byte nButton is the button-number (first button is number 0)
+  DBG_OUTPUT_PORT.print(F("Button "));
+  DBG_OUTPUT_PORT.print(nButton);
+  DBG_OUTPUT_PORT.println(F(" click."));
+}
+
+
+void mxD1_multi_setup()
+{
+  blink(3);
+
+  DBG_OUTPUT_PORT.println(F("TM1637 4 x 7-segment Display"));
+  display.setIntensity(7);
+  display.println("boot");    // F("xyz") macro doesn't work for TM16xxDisplay
+  //delay(5000);
+  display.setIntensity(1);
+
+  DBG_OUTPUT_PORT.println(F("TM1637 2 x RGB"));
+  rgbSet(1, RGB_YELLOW);      // set RGB1 to green
+  rgbSet(2, RGB_MAGENTA);      // set RGB2 to purple
+
+  buttons.attachClick(fnClick);
+
+  mxD1_multi_setupPCA();
+  pcfLedsSetup();
+
+  playTune("T240 L16 O8 FEDCDEF r1 "); // give a short blurp
+}
+
+void mxD1_multi_loop()
+{
+  //blink(1,10);
+  buttons.tick();
+
+  // update the leds on the PCF port
+  pcfLedsLoop();
+
+
+  // print remainder of scrolling text
+  bool fPrintingScrolled=TM1637PrintScrolled();
+
+
+}
